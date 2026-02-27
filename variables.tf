27689ea53f17da8b029bdbff0c8f3cf374ceeff5
rahaman <rahaman.abdoul786@gmail.com>
2026-02-27 01:29:30 +0100

    feat: S3 backend + required_providers + vars 2

--- variables.tf
+++ variables.tf
@@ -1,18 +1,20 @@
 # variables.tf
-variable "instance_type" {
-  default     = "t3.nano"
-  description = "type d'instance pour le serveur web"
-}
-variable "region" {
-  default     = "eu-central-1"
-  description = "region choisie"
-}
 variable "ami" {
-  default     = "ami-074dd8e8dac7651a5"
   description = "ami pour la region choisie"
+  default     = "ami-074dd8e8dac7651a5"
 }
 
 variable "environment" {
-  default     = "min"
   description = "environnement choisi"
+  default     = "min"
+}
+
+variable "instance_type" {
+  description = "type d'instance pour le serveur web"
+  default     = "t3.nano"
+}
+
+variable "region" {
+  description = "region choisie"
+  default     = "eu-central-1"
 }
